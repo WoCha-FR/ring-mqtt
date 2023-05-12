617d67aa8a224cbb9a26c263e610faa86ef59b3d
WoCha-FR <wocha@wocha.fr>
2023-05-12 13:32:21 +0200

    Modifications pour Jeedom

--- devices/security-panel.js
+++ devices/security-panel.js
@@ -36,12 +36,6 @@ export default class SecurityPanel extends RingSocketDevice {
     }
 
     publishState(data) {
-        const isPublish = data === undefined ? true : false
-        if (isPublish) {
-            // Eventually remove this but for now this attempts to delete the old light component based volume control from Home Assistant
-            this.mqttPublish(`homeassistant/switch/${this.locationId}/${this.deviceId}_bypass/config`, '', false)
-        }
-
         let alarmMode
         const alarmInfo = this.device.data.alarmInfo ? this.device.data.alarmInfo : []
 
