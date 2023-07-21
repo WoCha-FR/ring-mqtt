3769275b05dfd45bae057ba5d4d394c23e3dedf4
WoCha-FR <wocha@wocha.fr>
2023-07-21 10:14:54 +0200

    Remplacement heartbeat par device alerts

--- devices/base-polled-device.js
+++ devices/base-polled-device.js
@@ -16,14 +16,27 @@ export default class RingPolledDevice extends RingDevice {
         }
 
         this.device.onData.subscribe((data) => {
-            // Reset heartbeat counter on every polled state
-            this.heartbeat = 3
+            // Monitor Online-Offline
+            if (data.hasOwnProperty('alerts')) {
+                this.monitorAlerts(data.alerts)
+            }
+           // Reset heartbeat counter on every polled state
             if (this.isOnline()) { this.publishState(data) }
         })
-
-        this.monitorHeartbeat()
     }
 
+    // Monitor Connection alerts
+    async monitorAlerts(alerts) {
+        // Offline
+        if ($this.availabilityState !== 'offline' && alerts.connection === 'offline') {
+            this.offline()
+        }
+        // Online
+        if (this.availabilityState !== 'online' && alerts.connection === 'online') {
+            await this.online()
+        }
+
+    }
     // Publish device discovery, set online, and send all state data
     async publish() {
         await this.publishDiscovery()
@@ -31,34 +44,6 @@ export default class RingPolledDevice extends RingDevice {
         await utils.sleep(2)
         await this.online()
         this.publishState()
-    }
-
-    // This is a simple heartbeat function for devices which use polling.  This
-    // function decrements the heartbeat counter every 20 seconds.  In normal operation
-    // the heartbeat is constantly reset in the data publish function due to data
-    // polling events however, if something interrupts the connection, polling stops
-    // and this function will decrement until the heartbeat reaches zero.  In this case
-    // this function sets the device status offline.  When polling resumes the heartbeat
-    // is set > 0 and this function will set the device back online after a short delay.
-    async monitorHeartbeat() {
-        if (this.heartbeat > 0) {
-            if (this.availabilityState !== 'online') {
-                // If device was offline wait 10 seconds and check again, if still offline
-                // put device online.  Useful for initial startup or republish scenarios
-                // as publish will forcelly put the device online.
-                await utils.sleep(10)
-                if (this.heartbeat > 0 && this.availabilityState !== 'online') {
-                    await this.online()
-                }
-            }
-            this.heartbeat--
-        } else {
-            if (this.availabilityState !== 'offline') {
-                this.offline()
-            }
-        }
-        await utils.sleep(20)
-        this.monitorHeartbeat()
     }
 
     async getDeviceHistory(options) {
