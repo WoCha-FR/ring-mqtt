0e46bfa9b335fcd8bab501b34a02f5df5d0da152
WoCha-FR <wocha@wocha.fr>
2023-10-31 14:07:28 +0100

    HA vers Jeedom

--- devices/base-ring-device.js
+++ devices/base-ring-device.js
@@ -159,8 +159,8 @@ export default class RingDevice {
                 device: this.deviceData
             }
 
-            const configTopic = `homeassistant/${entity.component}/${this.locationId}/${this.deviceId}_${entityKey}/config`
-            this.debug(`HASS config topic: ${configTopic}`, 'disc')
+            const configTopic = `${utils.config().ring_topic}/config/${this.deviceId}/${entity.component}/${entityKey}`
+            this.debug(`Jeedom config topic: ${configTopic}`, 'disc')
             this.debug(discoveryMessage, 'disc')
             this.mqttPublish(configTopic, JSON.stringify(discoveryMessage), false)
 
